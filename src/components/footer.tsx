97dea130ff765fb569210f07dab826df74936799
Takalog <77995116+Takalog@users.noreply.github.com>
2021-01-26 07:36:55 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,21 +3,16 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
+      <span>More Takalog</span>
+      <ExtLink href="https://takalog.vercel.app/blog">
         <img
           src="https://vercel.com/button"
           height={46}
           width={132}
-          alt="deploy to Vercel button"
+          alt="Takalog to Vercel button"
         />
       </ExtLink>
-      <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
-        </ExtLink>
-      </span>
+
     </footer>
   </>
 )
